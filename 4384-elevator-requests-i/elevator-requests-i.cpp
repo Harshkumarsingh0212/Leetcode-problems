cfc2e58cbfd0634b11d4cc66745087e2dd6f15f9
Harshkumarsingh0212 <rajputharsh1718@gmail.com>
2026-08-16 21:17:26 +0530

    Time: 0 ms (100.00%) | Memory: 30.9 MB (100.00%) - LeetSync

---
+++ 4384-elevator-requests-i/elevator-requests-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int elevatorRequests(int n, vector<int>& requests) {
+        int curr=0;
+        int ans=0;
+        for(int i=0;i<requests.size();i++)
+        {
+         ans+=abs(requests[i]-curr);
+         curr=requests[i];
+        }
+        return ans;
+    }
+};
