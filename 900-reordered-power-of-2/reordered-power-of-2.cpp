7411c796ff294f48fc7fc5b8f98cbd08bed6a74e
Harshkumarsingh0212 <rajputharsh1718@gmail.com>
2026-08-28 23:23:43 +0530

    Time: 6 ms (10.87%) | Memory: 9.4 MB (12.90%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        unordered_map<int,int>mp;
+        while(n>0)
+        {
+            int rem=n%10;
+            mp[rem]++;
+            n/=10;
+            
+        }
+        for(int i=0;i<31;i++)
+        {
+           int res = 1 << i;
+            unordered_map<int,int>mp1;
+            while(res>0)
+            {
+                mp1[res%10]++;
+                res/=10;
+            }
+            if(mp1==mp) return true;
+        }
+        return false;
+    }
+};
