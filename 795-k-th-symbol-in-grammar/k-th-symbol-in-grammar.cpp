fc448d1c4f9debadde8b565cfb024e41b4a4a1fa
Harshkumarsingh0212 <rajputharsh1718@gmail.com>
2026-09-10 16:28:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (48.91%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -1,7 +1,10 @@
 class Solution {
     void solve(int n,char prev,int k,string&temp)
     {
-        if(n==0){ temp+=prev; return; }
+        if(n==0)
+        { temp+=prev;
+         return; 
+         }
         int half=1<<(n-1);
         if(k<=half) solve(n-1,prev,k,temp);
         else if(prev=='0') solve(n-1,'1',k-half,temp);
